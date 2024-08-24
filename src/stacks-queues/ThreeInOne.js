7af04984d13a5be4ac260c1e2337e4dd9d616132
terrencejihoonjung <terrencejung928@gmail.com>
2024-08-23 18:39:25 -0700

    reviewed ThreeInOne

--- src/stacks-queues/ThreeInOne.js
+++ src/stacks-queues/ThreeInOne.js
@@ -1,58 +1,68 @@
-var FixedMultiStack = /** @class */ (function () {
-    function FixedMultiStack(stackCapacity) {
-        this.numStacks = 3;
+var MultiStack = /** @class */ (function () {
+    function MultiStack(stackNum, stackCapacity) {
+        if (stackCapacity === void 0) { stackCapacity = 3; }
+        this.stackNum = stackNum;
         this.stackCapacity = stackCapacity;
-        this.values = new Array(this.numStacks * this.stackCapacity).fill(null);
-        this.sizes = new Array(this.numStacks).fill(0);
+        this.sizes = new Array(this.stackNum).fill(0);
+        this.values = new Array(this.stackNum * this.stackCapacity).fill(null);
     }
-    FixedMultiStack.prototype.push = function (stackNum, value) {
-        if (this.isFull(stackNum)) {
-            throw new Error("Stack ".concat(stackNum, " is full"));
-        }
+    MultiStack.prototype.push = function (stackNum, value) {
+        // check if stack is full
+        if (this.isFull(stackNum))
+            throw new Error("Cannot push. Stack ".concat(stackNum, " is full"));
+        // push value
+        var index = this.getTopIndex(stackNum);
+        this.values[index + 1] = value;
         this.sizes[stackNum]++;
-        var topIndex = this.indexOfTop(stackNum);
-        this.values[topIndex] = value;
     };
-    FixedMultiStack.prototype.pop = function (stackNum) {
-        if (this.isEmpty(stackNum)) {
-            throw new Error("Stack ".concat(stackNum, " is empty"));
-        }
-        var topIndex = this.indexOfTop(stackNum);
-        var value = this.values[topIndex];
-        this.values[topIndex] = null;
+    MultiStack.prototype.pop = function (stackNum) {
+        // check if stack is empty
+        if (this.isEmpty(stackNum))
+            throw new Error("Cannot pop. Stack ".concat(stackNum, " is empty"));
+        // pop value
+        var index = this.getTopIndex(stackNum);
+        this.values[index] = null;
         this.sizes[stackNum]--;
-        return value;
     };
-    FixedMultiStack.prototype.peek = function (stackNum) {
-        if (this.isEmpty(stackNum)) {
-            return null;
-        }
-        var topIndex = this.indexOfTop(stackNum);
-        return this.values[topIndex];
+    MultiStack.prototype.peek = function (stackNum) {
+        return this.values[this.getTopIndex(stackNum)];
     };
-    FixedMultiStack.prototype.isEmpty = function (stackNum) {
-        return this.sizes[stackNum] === 0;
-    };
-    FixedMultiStack.prototype.isFull = function (stackNum) {
+    MultiStack.prototype.isFull = function (stackNum) {
         return this.sizes[stackNum] === this.stackCapacity;
     };
-    FixedMultiStack.prototype.indexOfTop = function (stackNum) {
-        var offset = stackNum * this.stackCapacity;
-        var size = this.sizes[stackNum];
-        return offset + size - 1;
+    MultiStack.prototype.isEmpty = function (stackNum) {
+        return this.sizes[stackNum] === 0;
     };
-    return FixedMultiStack;
+    MultiStack.prototype.getTopIndex = function (stackNum) {
+        var size = this.sizes[stackNum];
+        var offset = this.stackCapacity * stackNum;
+        return size + offset - 1;
+    };
+    return MultiStack;
 }());
-var multiStack = new FixedMultiStack(3);
-multiStack.push(1, 2);
-multiStack.push(1, 3);
-multiStack.push(1, 1);
-console.log(multiStack.indexOfTop(1), multiStack.peek(1)); // Should output: 5 1
+var multiStack = new MultiStack(3);
+multiStack.push(0, 1);
+multiStack.push(0, 2);
+multiStack.push(0, 3);
+multiStack.push(1, 4);
+multiStack.push(1, 5);
+multiStack.push(1, 6);
+multiStack.push(2, 7);
+multiStack.push(2, 8);
+multiStack.push(2, 9);
+console.log(multiStack.peek(0)); // 3
+console.log(multiStack.peek(1)); // 6
+console.log(multiStack.peek(2)); // 9
+multiStack.push(0, 10);
+multiStack.pop(0);
+multiStack.pop(1);
+multiStack.pop(2);
+console.log(multiStack.peek(0)); // 2
+console.log(multiStack.peek(1)); // 5
+console.log(multiStack.peek(2)); // 8
 /**
 Describe how you could use a single array to implement three stacks.
 
 - designate three sections of the array as the three stacks
-- each section is dynamically sized
-- circular array?
--
+- numStack (number of stacks), stack capacity, values, sizes (helps keep track of ratio of slots)
 */
--- src/stacks-queues/ThreeInOne.ts
+++ src/stacks-queues/ThreeInOne.ts
@@ -1,73 +1,85 @@
-class FixedMultiStack {
-  private numStacks: number;
+class MultiStack {
+  private stackNum: number;
   private stackCapacity: number;
-  private values: (number | null)[];
   private sizes: number[];
+  private values: (number | null)[];
 
-  constructor(stackCapacity: number) {
-    this.numStacks = 3;
+  constructor(stackNum: number, stackCapacity: number = 3) {
+    this.stackNum = stackNum;
     this.stackCapacity = stackCapacity;
-    this.values = new Array(this.numStacks * this.stackCapacity).fill(null);
-    this.sizes = new Array(this.numStacks).fill(0);
+    this.sizes = new Array(this.stackNum).fill(0);
+    this.values = new Array(this.stackNum * this.stackCapacity).fill(null);
   }
 
-  push(stackNum: number, value: number): void {
-    if (this.isFull(stackNum)) {
-      throw new Error(`Stack ${stackNum} is full`);
-    }
+  push(stackNum: number, value: number) {
+    // check if stack is full
+    if (this.isFull(stackNum))
+      throw new Error(`Cannot push. Stack ${stackNum} is full`);
 
+    // push value
+    const index = this.getTopIndex(stackNum);
+    this.values[index + 1] = value;
     this.sizes[stackNum]++;
-    const topIndex = this.indexOfTop(stackNum);
-    this.values[topIndex] = value;
   }
 
-  pop(stackNum: number): number | null {
-    if (this.isEmpty(stackNum)) {
-      throw new Error(`Stack ${stackNum} is empty`);
-    }
+  pop(stackNum: number) {
+    // check if stack is empty
+    if (this.isEmpty(stackNum))
+      throw new Error(`Cannot pop. Stack ${stackNum} is empty`);
 
-    const topIndex = this.indexOfTop(stackNum);
-    const value = this.values[topIndex];
-    this.values[topIndex] = null;
+    // pop value
+    const index = this.getTopIndex(stackNum);
+    this.values[index] = null;
     this.sizes[stackNum]--;
-    return value;
   }
 
-  peek(stackNum: number): number | null {
-    if (this.isEmpty(stackNum)) {
-      return null;
-    }
-    const topIndex = this.indexOfTop(stackNum);
-    return this.values[topIndex];
+  peek(stackNum: number) {
+    return this.values[this.getTopIndex(stackNum)];
   }
 
-  isEmpty(stackNum: number): boolean {
-    return this.sizes[stackNum] === 0;
-  }
-
-  isFull(stackNum: number): boolean {
+  isFull(stackNum: number) {
     return this.sizes[stackNum] === this.stackCapacity;
   }
 
-  indexOfTop(stackNum: number): number {
-    const offset = stackNum * this.stackCapacity;
+  isEmpty(stackNum: number) {
+    return this.sizes[stackNum] === 0;
+  }
+
+  getTopIndex(stackNum: number) {
     const size = this.sizes[stackNum];
-    return offset + size - 1;
+    const offset = this.stackCapacity * stackNum;
+    return size + offset - 1;
   }
 }
 
-const multiStack = new FixedMultiStack(3);
-multiStack.push(1, 2);
-multiStack.push(1, 3);
-multiStack.push(1, 1);
+const multiStack = new MultiStack(3);
+multiStack.push(0, 1);
+multiStack.push(0, 2);
+multiStack.push(0, 3);
 
-console.log(multiStack.indexOfTop(1), multiStack.peek(1)); // Should output: 5 1
+multiStack.push(1, 4);
+multiStack.push(1, 5);
+multiStack.push(1, 6);
+
+multiStack.push(2, 7);
+multiStack.push(2, 8);
+multiStack.push(2, 9);
+
+console.log(multiStack.peek(0)); // 3
+console.log(multiStack.peek(1)); // 6
+console.log(multiStack.peek(2)); // 9
+
+multiStack.pop(0);
+multiStack.pop(1);
+multiStack.pop(2);
+
+console.log(multiStack.peek(0)); // 2
+console.log(multiStack.peek(1)); // 5
+console.log(multiStack.peek(2)); // 8
 
 /**
 Describe how you could use a single array to implement three stacks.
 
 - designate three sections of the array as the three stacks
-- each section is dynamically sized
-- circular array?
-- 
+- numStack (number of stacks), stack capacity, values, sizes (helps keep track of ratio of slots) 
 */
